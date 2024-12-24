398518a3b1f8909567599cbac45b7ba390c65a19
Alex Shelton <39957709+alexshelto@users.noreply.github.com>
2024-12-23 23:30:50 -0500

    pointless comments

--- lua/boringcomment/commenter.lua
+++ lua/boringcomment/commenter.lua
@@ -1,5 +1,7 @@
 local M = {}
 
+--- Comments visual section, driver method used by keymap
+-- This function takes line positions of a visual block and calls process lines
 M.comment_visual_selection = function()
     local current_win = vim.api.nvim_get_current_win()
     local from = vim.fn.line("v", current_win)
@@ -18,6 +20,8 @@ M.comment_visual_selection = function()
 end
 
 
+--- Comments for normal mode, driver method used by keymap
+-- This function takes current line position and calls process lines
 M.comment_current_line = function ()
     local start_line_num = vim.fn.line('.')
     local end_line_num = start_line_num + 1
@@ -25,19 +29,22 @@ M.comment_current_line = function ()
     M.process_lines(start_line_num, end_line_num)
 end
 
+---process the lines, comment or uncomment the line numbers
+---@param start_line_num number the start line number to comment from
+---@param end_line_num number the end line number to comment to
 M.process_lines = function(start_line_num, end_line_num)
     local current_buf = vim.api.nvim_get_current_buf()
 
     local comment_prefix = vim.api.nvim_buf_get_option(current_buf, "commentstring")
     local cleaned_comment_prefix = comment_prefix:gsub("%%s", "") -- Removes special chars
 
-    -- Pull lines out of buffer
+    -- Pull lines from buffer
     local lines = vim.api.nvim_buf_get_lines(current_buf, start_line_num-1, end_line_num-1, false)
 
     -- Determine if lines are already commented (every line starts with a comment) if not we comment
     local is_commented = M.is_already_commented(lines, cleaned_comment_prefix)
 
-    local replace_lines -- lines that will contain comment or uncomment strings to replace
+    local replace_lines
 
     if  is_commented then
         replace_lines = M.create_uncomment_lines(lines, cleaned_comment_prefix)
@@ -54,7 +61,10 @@ M.process_lines = function(start_line_num, end_line_num)
     )
 end
 
-
+---Create uncomment lines: If selected lines are already commented, return uncommented
+---@param lines table[string] lines from buffer with comment string
+---@param cleaned_comment_prefix string comment string to remove from lines
+---@return table[string] of uncommented lines
 M.create_uncomment_lines = function(lines, cleaned_comment_prefix)
     local uncomment_pattern = "^%s*" .. vim.pesc(cleaned_comment_prefix)
     for idx, line in ipairs(lines) do
@@ -64,6 +74,10 @@ M.create_uncomment_lines = function(lines, cleaned_comment_prefix)
 end
 
 
+---Create comment lines: If selected lines are not commented, return commented
+---@param lines table[string] lines from buffer 
+---@param cleaned_comment_prefix string comment string to add to lines
+---@return table[string] of commented lines
 M.create_comment_lines = function(lines, comment_prefix)
     for idx, line in ipairs(lines) do
         lines[idx] = comment_prefix:format(line)
@@ -71,7 +85,10 @@ M.create_comment_lines = function(lines, comment_prefix)
     return lines
 end
 
-
+---Determine if lines are already commented
+---@param lines table[string] lines from buffer
+---@param comment_prefix string
+---@return boolean
 M.is_already_commented = function(lines, comment_prefix)
     local cleaned_comment_string = comment_prefix:gsub("%%s", "")
 
